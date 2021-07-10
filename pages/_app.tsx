e2efae8f19939a4b7ee84f9e6bb83da1db25da23
Sebastian Yesid Tabares Amaya <sytabaresa@gmail.com>
2021-07-10 14:34:38 -0500

    add i18n and WIP:PWA integration, closes #3, closes #4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import 'tailwindcss/tailwind.css'
+import { appWithTranslation } from 'next-i18next';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default appWithTranslation(MyApp)
