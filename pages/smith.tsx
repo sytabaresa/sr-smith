0e23d400e4eec15703f6eaed5d07281929a0c3d5
Sebastian Yesid Tabares Amaya <sytabaresa@gmail.com>
2021-07-11 23:47:28 -0500

    add initial parsing an execution methods, example code, closes #8, closes #9, closes #11, closes #13

--- pages/smith.tsx
+++ pages/smith.tsx
@@ -1,26 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import AppLayout from "../components/smith/AppLayout";
-import SmithBoard from "../components/smith/board";
+import SmithBoard from "../components/smith/Board";
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { SmithContext } from "../components/smith/context";
+import JCText from "../components/smith/Editor";
 
 const Smith: React.FC = () => {
-    return (
-        <AppLayout title="Smith Chart" >
-          <div className="h-screen flex">
-            <SmithBoard />
-          </div>
-        </AppLayout>
-    );
+  const [board, setBoard] = useState<any>(null);
+
+  const context = {
+    board,
+    boxName: 'smith-box',
+    setBoard
+  }
+
+  return (
+    <AppLayout title="Smith Chart" >
+      <SmithContext.Provider value={context}>
+        <div className="h-screen flex relative">
+          <SmithBoard />
+          <JCText className="absolute top-0 left-0 ml-4 mt-4 w-96" />
+        </div>
+      </SmithContext.Provider>
+    </AppLayout>
+  );
 }
 
 
 export async function getStaticProps({ locale }) {
-    return {
-      props: {
-        ...(await serverSideTranslations(locale, ['common', 'footer'])),
-        // Will be passed to the page component as props
-      },
-    };
-  }
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common', 'smith', 'footer'])),
+      // Will be passed to the page component as props
+    },
+  };
+}
 
 export default Smith;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or  false, 'media' or 'class'
   theme: {
     extend: {},
   },
