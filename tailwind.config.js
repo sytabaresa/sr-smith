8aac9ac7ad8be56a31560aa3410368a24213d2bb
Sebastian Yesid Tabares Amaya <sytabaresa@gmail.com>
2021-07-12 19:46:35 -0500

    add popup and tooltips support, closes #22, closes #23, closes #24

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or  false, 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        principal: {
+          DEFAULT: colors.blueGray[800],
+          ...colors.blueGray
+        },
+        contrast: colors.white,
+      }
+    },
   },
   variants: {
     extend: {},
