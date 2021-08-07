ecc46b91b9baac3a624dc6274d755ef1d971bffd
Sebastian Yesid Tabares Amaya <sytabaresa@gmail.com>
2021-08-07 12:50:10 -0500

    proof of concept of msith grid

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { appWithTranslation } from 'next-i18next';
 import { useEffect } from 'react';
 import 'tailwindcss/tailwind.css'
 import './main.css';
+import "jsxgraph/distrib/jsxgraph.css"
 
 function MyApp({ Component, pageProps }) {
 
