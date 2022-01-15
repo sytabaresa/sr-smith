2b3b8d2f556447832c1065cfa52901cbae8e4120
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2022-01-15 17:32:26 -0500

    change file path

--- components/atoms/editorButton.tsx
+++ components/atoms/editorButton.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Editor from "./Editor";
+import Editor from "../smith/Editor";
 import { useLayer, Arrow } from "react-laag";
 import { useTranslation } from "next-i18next";
 import { motion } from "framer-motion";
