fef6059aac8e0598f4968120f478795d1bd2203c
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2022-01-15 19:09:57 -0500

    create organism

---
+++ components/organisms/codeTools.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useContext } from "react";
+import EditorButton from "../atoms/editorButton";
+import { DotsHorizontalIcon } from "@heroicons/react/outline";
+
+const CodeTools = () => {
+  const { t } = useTranslation("smith");
+  return (
+    <div className="absolute top-0 left-0 pl-4 pt-4 w-full md:w-96 z-10">
+      <div className="form-control">
+        <div className="space-x-2 hidden md:flex">
+          <input
+            type="text"
+            placeholder={t("search")}
+            className="w-full input input-primary input-bordered"
+          />
+          <button className="btn btn-primary">{t("go")}</button>
+        </div>
+        <div className="form-control flex-row">
+          <label className="cursor-pointer label mr-6">
+            <span className="label-text font-bold mr-2">{t("smith-mode")}</span>
+            <div>
+              <input type="checkbox" className="toggle toggle-primary" />
+              <span className="toggle-mark"></span>
+            </div>
+          </label>
+          <label className="cursor-pointer label mr-6">
+            <span className="label-text font-bold mr-2">{t("option-2")}</span>
+            <div>
+              <input type="checkbox" className="toggle toggle-primary" />
+              <span className="toggle-mark"></span>
+            </div>
+          </label>
+        </div>
+      </div>
+      <div className="btn-group">
+        <EditorButton />
+        <button className="btn">
+          <DotsHorizontalIcon className="h-5 w-5" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CodeTools;
