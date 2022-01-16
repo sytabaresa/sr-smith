e6f799f21fc4d5a1ec6d2f446e35cce6769a6ca6
Sebastian Yesid Tabares Amaya <sytabaresa@gmail.com>
2022-01-15 19:08:50 -0500

    WIP: add firebase

---
+++ firebase/clientApp.ts
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+import 'firebase/firestore' // If you need it
+import 'firebase/storage' // If you need it
+import 'firebase/performance' // If you need it
+import { clientCredentials } from './config';
+
+
+console.log(clientCredentials)
+export const app = initializeApp(clientCredentials)
+export const analytics = getAnalytics(app)
+export const auth = getAuth(app)
+// Check that `window` is in scope for the analytics module!
+if (typeof window !== 'undefined') {
+    // Enable analytics. https://firebase.google.com/docs/analytics/get-started
+    // if ('measurementId' in clientCredentials) {
+    //   firebase.performance()
+    // }
+}
+
+
+export default app
