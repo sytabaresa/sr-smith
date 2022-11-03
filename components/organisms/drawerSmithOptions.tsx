6392718976303a7d2dc53c16195a4cc4a8dc2b7b
Sebastian Tabares <sytabaresa@gmail.com>
2022-11-03 17:56:29 -0500

    refactor modal menu for apps, change implementation of newProjectForm

--- components/organisms/drawerSmithOptions.tsx
+++ components/organisms/drawerSmithOptions.tsx
@@ -6,25 +6,69 @@ import {
     FolderOpenIcon,
     ShareIcon,
     CogIcon,
-    LogoutIcon
-  } from "@heroicons/react/outline";
+    LogoutIcon,
+    LoginIcon
+} from "@heroicons/react/outline";
+import { auth } from "../../firebase/clientApp";
+import { signOut } from "firebase/auth";
+import { useRouter } from "next/router";
+import { useUser } from "../../providers/userContext";
+import ModalContainer from "../molecules/modalContainer";
+import NewProjectForm from "./newProjectForm";
+import PublishProjectForm from "./publishProjectForm";
 
 
-const DrawerSmithOptions =() => {
-    return(
+const DrawerSmithOptions = () => {
+    const NEW_PROJECT_LABEL = 'new-project-modal'
+
+    const router = useRouter()
+    const { isAuthenticated } = useUser()
+
+    const logout = async () => {
+        try {
+            await signOut(auth)
+            router.push('/login')
+        } catch (err) {
+            console.log('logout error', err)
+        }
+    }
+
+    const login = () => {
+        router.push('/login')
+    }
+
+    const open = () => {
+        router.push('/saved')
+    }
+
+    return <>
         <div className="drawer-side">
             <label htmlFor="my-drawer" className="drawer-overlay"></label>
             <div className="p-4 overflow-y-auto w-52 bg-base-100 text-base-content flex flex-col items-start">
-                <DrawerMenuItem icon={<PlusIcon className="w-8 h-8" />} label="New" />
-                <DrawerMenuItem icon={<SaveIcon className="w-8 h-8" />} label="Save" />
-                <DrawerMenuItem icon={<FolderOpenIcon className="w-8 h-8" />} label="Open" />
+                <label htmlFor={NEW_PROJECT_LABEL}>
+                    <DrawerMenuItem icon={<PlusIcon className="w-8 h-8" />} label="New" />
+                </label>
+                {/* <DrawerMenuItem icon={<SaveIcon className="w-8 h-8" />} label="Save" /> */}
+
+                <DrawerMenuItem icon={<FolderOpenIcon className="w-8 h-8" />} label="Open" onClick={open} />
                 <DrawerMenuItem icon={<ShareIcon className="w-8 h-8" />} label="Share" />
                 <DrawerMenuItem icon={<ShareIcon className="w-8 h-8" />} label="Publish" />
                 <DrawerMenuItem icon={<CogIcon className="w-8 h-8" />} label="Settings" />
-                <DrawerMenuItem icon={<LogoutIcon className="w-8 h-8" />} label="Logout" />
+                {isAuthenticated ?
+                    <DrawerMenuItem icon={<LogoutIcon className="w-8 h-8" />} label="Logout" onClick={logout} />
+                    :
+                    <DrawerMenuItem icon={<LoginIcon className="w-8 h-8" />} label="Login" onClick={login} />
+                }
             </div>
-          </div>
-    )
+        </div>
+        <ModalContainer
+            className="w-10/12 md:w-3/12"
+            modalChild={<NewProjectForm />}
+            modalName={NEW_PROJECT_LABEL}
+            isModal
+        >
+        </ModalContainer>
+    </>
 }
 
 export default DrawerSmithOptions
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { db } from "../firebase/clientApp";
 import { SmithProject } from "../interfaces";
 import { getAuth } from "firebase/auth";
 import { useRouter } from "next/router";
-import ProjectSelector from "../components/molecules/projectSelector";
+import ModalContainer from "../components/molecules/modalContainer";
 import { FolderAddIcon, FolderIcon } from "@heroicons/react/outline";
 import NewProjectForm from "../components/organisms/newProjectForm";
 
@@ -13,46 +13,42 @@ const Projects = () => {
   const user = auth.currentUser;
   const router = useRouter();
 
-  const openNew = async (data) => {
-    const { projectName, projectDescription } = data;
-    try {
-      const docRef = await addDoc(collection(db, "projects"), {
-        createAt: Timestamp.now(),
-        data: "",
-        description: projectDescription,
-        hashReference: "",
-        isPubic: false,
-        name: projectName,
-        updateAt: Timestamp.now(),
-        userId: user.uid,
-      } as SmithProject);
-      router.push(`/canvas?id=${docRef.id}`);
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
-  };
-
   const goToSavedProjects = () => {
     router.push("/saved");
   };
 
+  const Child = ({ image, title }: { image: JSX.Element, title: string }) => {
+    return <div className="card py-10 bg-gray-300  shadow-2xl flex flex-col items-center justify-center">
+      <div className="text-lg font-semidbold text-center text-gray-500">
+        {image}
+        <span>
+          {title}
+        </span>
+      </div>
+    </div>
+  }
+
   return (
     <div className="flex flex-wrap items-center justify-center h-screen space-y-2 md:space-x-5 relative">
-      <ProjectSelector
-        title={"Nuevo Proyecto"}
-        image={
-          <FolderIcon className="h-10 w-10 md:h-20 md:w-20 text-gray-500" />
-        }
-        modalChild={<NewProjectForm onSubmit={openNew} />}
+      <ModalContainer
+        className="w-10/12 md:w-3/12"
+        modalChild={<NewProjectForm />}
         isModal
-      />
-      <ProjectSelector
-        title={"Abrir Proyecto"}
-        image={
-          <FolderAddIcon className="h-10 w-10 md:h-20 md:w-20 text-gray-500" />
-        }
+      >
+        <Child
+          title={"Nuevo Proyecto"}
+          image={<FolderIcon className="h-10 w-10 md:h-20 md:w-20 text-gray-500" />}
+        />
+      </ModalContainer>
+      <ModalContainer
+        className="w-10/12 md:w-3/12"
         onClick={goToSavedProjects}
-      />
+      >
+        <Child
+          title={"Abrir Proyecto"}
+          image={<FolderAddIcon className="h-10 w-10 md:h-20 md:w-20 text-gray-500" />}
+        />
+      </ModalContainer>
       <img
         src="../images/smith-app.png"
         alt="smith-bg"
